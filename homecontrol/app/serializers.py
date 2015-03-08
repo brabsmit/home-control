b8dffaea8ec22f0c08e7303f958f2830e9addad1
root <root@ip-172-31-23-243.us-west-1.compute.internal>
2015-03-08 21:30:35 +0000

    Fixed api bug that disabled patching

--- homecontrol/app/serializers.py
+++ homecontrol/app/serializers.py
@@ -8,37 +8,37 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
       fields = ('username',)
 
 class HomeSerializer(serializers.HyperlinkedModelSerializer):
-   owner = UserSerializer()
+   owner = UserSerializer(read_only=True)
    class Meta:
       model = Home
       fields = ('owner', 'name')
 
 class RoomSerializer(serializers.HyperlinkedModelSerializer):
-   home = HomeSerializer()
+   home = HomeSerializer(read_only=True)
    class Meta:
       model = Room
       fields = ('home', 'name', 'room_type')
 
 class LightSerializer(serializers.HyperlinkedModelSerializer):
-   room = RoomSerializer()
+   room = RoomSerializer(read_only=True)
    class Meta:
       model = Light
       fields = ('room', 'name', 'switch_mode', 'is_on','pk')
       
 class DoorSerializer(serializers.HyperlinkedModelSerializer):
-   room = RoomSerializer()
+   room = RoomSerializer(read_only=True)
    class Meta:
       model = Door
       fields = ('room', 'name', 'is_locked','pk')
 
 class RefrigeratorSerializer(serializers.HyperlinkedModelSerializer):
-   room = RoomSerializer()
+   room = RoomSerializer(read_only=True)
    class Meta:
       model = Refrigerator
       fields = ('room', 'name', 'fridge_set_temp','fridge_current_temp','freezer_set_temp','freezer_current_temp','pk')
       
 class ThermostatSerializer(serializers.HyperlinkedModelSerializer):
-   home = HomeSerializer()
+   home = HomeSerializer(read_only=True)
    class Meta:
       model = Thermostat
       fields = ('home', 'name', 'controls','current_temp','set_temp','pk')
