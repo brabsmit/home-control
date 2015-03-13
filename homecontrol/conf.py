f2ce0c24b129160d4c457d773a1a1fc0fa131353
root <root@ip-172-31-23-243.us-west-1.compute.internal>
2015-03-13 17:22:55 +0000

    Restructured docs

--- homecontrol/conf.py
+++ homecontrol/conf.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # django-homecontrol documentation build configuration file, created by
-# sphinx-quickstart on Fri Mar 13 17:17:49 2015.
+# sphinx-quickstart on Fri Mar 13 17:21:34 2015.
 #
 # This file is execfile()d with the current directory set to its
 # containing dir.
@@ -29,14 +29,7 @@ import shlex
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.doctest',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.coverage',
-    'sphinx.ext.mathjax',
-    'sphinx.ext.viewcode',
-]
+extensions = []
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -287,7 +280,3 @@ texinfo_documents = [
 
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #texinfo_no_detailmenu = False
-
-
-# Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'https://docs.python.org/': None}
--- homecontrol/index.rst
+++ homecontrol/index.rst
@@ -1,5 +1,5 @@
 .. django-homecontrol documentation master file, created by
-   sphinx-quickstart on Fri Mar 13 17:17:49 2015.
+   sphinx-quickstart on Fri Mar 13 17:21:34 2015.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
